ea7f49586028d012c55fa92f68a87cf6845a528a
rulitoxd <rulitosxd1@gmail.com>
2020-02-08 20:55:57 -0500

    BeginR2

--- branches1.py
+++ branches1.py
@@ -1,10 +1,15 @@
+
+# Rama2
 def function1(params):
-	return params*params
+	var = 0
+	for i in range(0,10):
+		var = var + params
+	return params*var
 	
 def function2(params):
-	return params+params
+	return 10+params
 
-def hola():
+def adios():
 	return 2
 	
 # Prueba de branches en git
